ed89d2e7d9664fcf4e179b757cdb7c3d634930b7
Md Sakil Khan <sakil2468@gmail.com>
2021-11-26 13:08:13 -0500

    Initial Java template
    Testing if color and emojis are working.

---
+++ TicketViewer.java
@@ -0,0 +1,13 @@
+/**
+ * Zendesk Ticket Viewer
+ *
+ * @author Md Sakil Khan
+ * @version 1.0
+ */
+
+public class TicketViewer{
+
+  public static void main(String []args){
+    System.out.println("\033[1;32mHi!!! 😊");
+  }
+}
